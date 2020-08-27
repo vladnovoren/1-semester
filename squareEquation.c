7f67b759e0a359a5b9ed91d2d740651bcb688ff4
vladnovoren <47698772+vladnovoren@users.noreply.github.com>
2020-08-27 22:28:30 +0300

    square equation solver

---
+++ squareEquation.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include <math.h>
+
+const int INF_SOL_FLAG=-1;
+
+int quadSolver(double a, double b, double c, double* x1, double* x2){
+    if(a==0){
+        if(b==0){
+            if(c==0){
+                return INF_SOL_FLAG;
+            }
+            else{
+                return 0;
+            }
+        }
+        else{//b!=0
+            *x1=*x2=-c/b;
+            return 1;
+        }
+    }
+    else{//a!=0
+        double discrim=b*b-4*a*c;
+
+        if(discrim<0){
+            return 0;
+        }
+
+        if(discrim==0){
+            *x1=*x2=-b/(2*a);
+            return 1;
+        }
+
+        if(discrim>0){
+            *x1=(-b-sqrt(discrim))/(2*a);
+            *x2=-b-*x1;
+            return 2;
+        }
+    }
+}
+
+
+int main(){
+
+    double x1=0, x2=0, a=0, b=0, c=0;
+
+    printf("Hello! This is quadratic equation solver\n");
+
+    printf("Please enter coefficients:\n");
+
+    printf("a: ");
+    scanf("%lg", &a);
+
+    printf("b: ");
+    scanf("%lg", &b);
+
+    printf("c: ");
+    scanf("%lg", &c);
+
+    int countSolution=quadSolver(a, b, c, &x1, &x2);
+
+    switch(countSolution){
+        case INF_SOL_FLAG:
+            printf("Infinite number of solutions\n");
+            break;
+
+        case 0:
+            printf("No solutions :(\n");
+            break;
+
+        case 1:
+            printf("One solution: %lg\n", x1);
+            break;
+
+        case 2:
+            printf("Two solutions: %lg %lg\n", x1, x2);
+            break;
+
+        default:
+            printf("Error in: main(): countSolution=%d\n", countSolution);
+            return 1;
+
+    }
+
+    return 0;
+}
